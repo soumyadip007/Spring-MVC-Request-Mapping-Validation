d848ec9c412c437e6c8db7e3da64b1b055d0aaea
soumyadip007 <soumyadip.note@gmail.com>
2019-05-22 18:31:08 +0530

    Bean class initalized

---
+++ Spring MVC/src/com/udemy/spring/form/Student.java
@@ -0,0 +1,29 @@
+package com.udemy.spring.form;
+
+public class Student {
+
+	public String firstname;
+	public String lastname;
+	
+	public Student()
+	{
+		
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+}
