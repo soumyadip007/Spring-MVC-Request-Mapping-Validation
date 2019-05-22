a5909b89beefca1da001a1de20d8aeb0554bd2b7
soumyadip007 <soumyadip.note@gmail.com>
2019-05-23 00:24:52 +0530

    Validation Bug Fixed only Customer.jsp connection bug remains

--- Spring MVC/src/com/udemy/spring/validation/customerController.java
+++ Spring MVC/src/com/udemy/spring/validation/customerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.udemy.spring.form.Student;
 
 @Controller
-@RequestMapping("/customerr")
+@RequestMapping("/customer")
 public class customerController {
 
 	
@@ -34,11 +34,13 @@ public class customerController {
 				System.out.println("First");
 
 				return "customer-form";
+			
+				
 			}
 			else {
 
 				System.out.println("2nd");
-
+				
 				return "customer";
 			}
 		}
