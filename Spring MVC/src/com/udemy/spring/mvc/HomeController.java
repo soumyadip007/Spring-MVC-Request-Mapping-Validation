51d9e826d99e137e976310240044c3d80caf7e0d
soumyadip007 <soumyadip.note@gmail.com>
2019-05-22 14:50:02 +0530

    Controller Methd (@RequestMapping)

--- Spring MVC/src/com/udemy/spring/mvc/HomeController.java
+++ Spring MVC/src/com/udemy/spring/mvc/HomeController.java
@@ -1,8 +1,15 @@
 package com.udemy.spring.mvc;
 
 import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+	@RequestMapping("/")
+	public String ShowPage()
+	{
+		return null;
+		
+	}
 }
